0404b99bacdc1f7da2b4bd70b9b3bed41928bbf7
Antanas <antanas.slusnys@teltonika.lt>
2026-05-09 11:02:38 +0300

    fix: update layout flex properties to enable horizontal scrolling in editor toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -431,7 +431,7 @@ export default function App() {
         )}
 
         <main className="flex-1 flex flex-col overflow-hidden bg-transparent relative min-h-0">
-          <div className="flex-1 overflow-hidden relative min-h-0 bg-background/20">
+          <div className="flex-1 flex flex-col overflow-hidden relative min-h-0 bg-background/20">
             <Editor />
           </div>
           <StatusBar />
